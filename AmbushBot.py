73bc40e244f8452ebe1206aba1faa2997fb545fa
Toh Nic <tohnic1999@gmail.com>
2019-10-20 14:25:51 +0800

    initializing ambush bot

---
+++ AmbushBot.py
@@ -0,0 +1,58 @@
+from os import environ
+from telethon import TelegramClient, events
+from telethon.tl.types import PeerUser
+from telethon.tl.functions.messages import SendMessageRequest
+from alchemysession import AlchemySessionContainer
+
+import asyncio
+
+import logging
+import configparser
+import datetime
+import time
+import random
+import sys
+import json
+import re
+
+
+
+admonid = 323232619 
+ambushChannelID = "-1001193142189"
+botToken = "584961873:AAHfUZsieS6U4NcCBf2WlPU910ehXbbQWQM"
+website = "https://api.telegram.org/bot"+botToken
+cwBotChat = 'chtwrsbot'
+semaphore = 1
+semaphore2 = 1
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+container = AlchemySessionContainer(environ['DATABASE_URL'])
+session_name = environ.get('TG_SESSION', 'session')
+container.core_mode = True 
+session = container.new_session(session_name)
+
+botToken = environ['TG_BOT_TOKEN']
+client = TelegramClient(
+    session, int(environ['TG_API_ID']), environ['TG_API_HASH'])
+
+client.start(bot_token = botToken)
+
+@client.on(events.ChatAction)
+async def validateJoin(event):
+    if event.user_added:
+        user = await event.get_added_by()
+        if user.id != admonid:
+            chatID = event.chat_id
+            await client.delete_dialog(chatID)
+
+
+
+
+
+
+
+
+
